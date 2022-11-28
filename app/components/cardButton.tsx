3509a52e98ce890f9c420fb98d43894fe57764ec
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2022-11-28 22:50:41 +0900

    components/cardButton.tsxを定義

---
+++ app/components/cardButton.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import { CardActionArea, Card, CardMedia, CardContent, Typography, Grid } from '@mui/material'
+import { Character } from '../interfaces'
+
+const CardButton = ({
+  id,
+  name,
+  onClick,
+}: Character & {
+  onClick: { (id: string): void }
+}) => {
+  const image = name ? `/${name}.png` : '/ミホノブルボン.png'
+  return (
+    <CardActionArea onClick={() => onClick(id)}>
+      <Card>
+        <CardMedia>
+          <Image src={image} height={140} width={140} priority={true} loading='eager' />
+        </CardMedia>
+        <CardContent>
+          <Typography>{name}</Typography>
+        </CardContent>
+      </Card>
+    </CardActionArea>
+  )
+}
+
+export default CardButton
