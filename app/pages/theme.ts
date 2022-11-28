4d2556fff897a9a90e80b1b1e4aa27cc2c18cb14
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2022-11-28 22:38:49 +0900

    共通レイアウトを定義

--- app/pages/theme.ts
+++ app/pages/theme.ts
@@ -2,6 +2,16 @@ import { createTheme } from '@mui/material/styles'
 
 const theme = createTheme({
   // TODO:テーマを設定する
+  palette: {
+    primary: {
+      main: '#009688',
+      contrastText: '#795548',
+    },
+    background: {
+      default: '#bdbdbd',
+    },
+    text: { primary: '#ff9800' },
+  },
 })
 
 export default theme
