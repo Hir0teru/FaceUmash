31c6c15eca8cb1654be3f5f07bb8b47e26db3337
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2023-05-02 22:27:19 +0900

    ヘッダー押下時にトップページへ遷移する修正

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,18 +1,21 @@
 import { AppBar, Toolbar, Typography, Box } from '@mui/material'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = (): JSX.Element => {
   return (
     <>
-      <AppBar position='static' sx={{ height: '100%' }}>
-        <Box sx={{ margin: 'auto' }}>
-          <Toolbar variant='dense'>
-            <Typography variant='h4' component='div'>
-              FaceUmash
-            </Typography>
-          </Toolbar>
-        </Box>
-      </AppBar>
+      <Link href='/' style={{ textDecoration: 'none' }}>
+        <AppBar position='static' sx={{ height: '100%' }}>
+          <Box sx={{ margin: 'auto' }}>
+            <Toolbar variant='dense'>
+              <Typography variant='h4' component='h2'>
+                FaceUmash
+              </Typography>
+            </Toolbar>
+          </Box>
+        </AppBar>
+      </Link>
     </>
   )
 }
