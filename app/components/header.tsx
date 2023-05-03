d16083bb136edf631f5509910011e78e7a80bb72
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2023-05-03 18:29:37 +0900

    headerに表示する文言を修正

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,14 +6,17 @@ const Header = (): JSX.Element => {
   return (
     <>
       <Link href='/' style={{ textDecoration: 'none' }}>
-        <AppBar position='static' sx={{ height: '100%' }}>
-          <Box sx={{ margin: 'auto' }}>
-            <Toolbar variant='dense'>
-              <Typography variant='h4' component='h2'>
-                FaceUmash
-              </Typography>
-            </Toolbar>
-          </Box>
+        <AppBar position='static' sx={{ textAlign: 'center' }}>
+          <Toolbar variant='dense'>
+            <Box sx={{ margin: 'auto' }}>
+              <Box>
+                <Typography variant='h4'>faceUmash</Typography>
+              </Box>
+              <Box>
+                <Typography variant='subtitle1'>みんなでつくるウマ娘ランキング</Typography>
+              </Box>
+            </Box>
+          </Toolbar>
         </AppBar>
       </Link>
     </>
