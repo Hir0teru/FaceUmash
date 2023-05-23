378b3e0c1c73601c5067f1350bb62f0608125538
hiroteru <78360123+HiraiDaiki@users.noreply.github.com>
2023-05-23 22:53:46 +0900

    fix header.test.tsx

--- app/components/__tests__/header.test.tsx
+++ app/components/__tests__/header.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Header from '../header'
 
-test('Test for footer.tsx', () => {
+test('Test for header.tsx', () => {
   render(<Header />)
-  const element = screen.getByText('FaceUmash')
+  const element = screen.getByText('faceUmash')
   expect(element).toBeInTheDocument()
 })
